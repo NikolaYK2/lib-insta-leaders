58ac9245a5de8c27c44b83cae6f83463d961107f
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-08-29 12:50:22 +0400

    init: del dist

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { JSX as JSX_2 } from 'react/jsx-runtime';
-
-export declare function CardTest({ color }: CardTestProps): JSX_2.Element;
-
-export declare type CardTestProps = {
-    color?: "red" | "green" | "blue";
-};
-
-export { }
