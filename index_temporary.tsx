4d18c81d11fb751559c89c1f4f08d1159a0850cc
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-09-03 01:21:01 +0400

    fix: package.json (add @radix-ui/react-dropdown-menu)

--- index_temporary.tsx
+++ index_temporary.tsx
@@ -1,9 +1,10 @@
 import {createRoot} from "react-dom/client";
-import {Typography} from "./src";
+import {Card} from "./src";
 import {TypographyVariant} from "./src/components/typography/typography.enum";
 
 createRoot(document.getElementById("root")!).render(
     <div>
         <b>This is Library of It-Leaders (Temporary)</b>
+        <Card>hello</Card>
     </div>
 );
