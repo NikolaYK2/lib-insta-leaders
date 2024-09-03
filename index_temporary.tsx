f223c1ee517b728effbc9a6108dc6320b3b499b4
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-09-03 23:06:41 +0400

    fix: IconsBrowser component

--- index_temporary.tsx
+++ index_temporary.tsx
@@ -1,10 +1,13 @@
 import {createRoot} from "react-dom/client";
-import {Card} from "./src";
+import {Card, IconBrowser} from "./src";
 import {TypographyVariant} from "./src/components/typography/typography.enum";
 
 createRoot(document.getElementById("root")!).render(
     <div>
         <b>This is Library of It-Leaders (Temporary)</b>
         <Card>hello</Card>
+        <IconBrowser iconId={"microsoft-edge"}/>
+        <Card>hello</Card>
+
     </div>
 );
--- src/components/icons/iconsBrowser/iconsBrowser.tsx
+++ src/components/icons/iconsBrowser/iconsBrowser.tsx
@@ -1,4 +1,4 @@
-import iconsSprite from './iconsSprite.svg'
+import {iconsSprite} from './iconsSprite.tsx'
 
 export interface Props {
     iconId: IconId;
@@ -11,10 +11,12 @@ export interface Props {
 export type IconId = 'brave' | 'explorer' | 'firefox' | 'microsoft-edge' | 'opera' | 'safari' | 'uc-browser' | 'yandex'
 
 export const IconBrowser = (props: Props) => {
-    return (
-        <svg width={props.width || "36"} height={props.height || "36"} viewBox={props.viewBox || "0 0 36 36"}
-             fill={props.fill || "#ffffff"} xmlns="http://www.w3.org/2000/svg">
-            <use xlinkHref={`${iconsSprite}#${props.iconId}`}/>
-        </svg>
+    return (<>
+            {iconsSprite}
+            <svg width={props.width || "36"} height={props.height || "36"} viewBox={props.viewBox || "0 0 36 36"}
+                 fill={props.fill || "#ffffff"} xmlns="http://www.w3.org/2000/svg">
+                <use xlinkHref={`#${props.iconId}`}/>
+            </svg>
+        </>
     );
 };
