2551ddb1ac8ce29a22b7eedbd44a1e6dfa114eb4
albogachiev <albogachiev-2014@mail.ru>
2024-09-04 13:56:58 +0300

    IL-51 fix: DropDownMenu, DynamicMenuItem, styles

--- src/components/dropDownMenu/dynamicMenuItem/DynamicMenuItem.tsx
+++ src/components/dropDownMenu/dynamicMenuItem/DynamicMenuItem.tsx
@@ -1,44 +1,45 @@
-import {Icon} from "../../icon/icon.tsx";
-import s from "./styles.module.scss";
-import {forwardRef} from "react";
+// import {Icon} from "../../icon/icon.tsx";
+import s from './styles.module.scss'
+import { forwardRef } from 'react'
 
-export type IconProps = 'logOut'
-    | 'profile'
-    | 'play'
-    | 'trash'
-    | 'edit'
+export type IconProps = 'logOut' | 'profile' | 'play' | 'trash' | 'edit'
 
 type AnchorProps = {
-    type: 'a';
-    route: string;
+  type: 'a'
+  href: string
 }
 type ButtonProps = {
-    type: 'button';
-    callback: () => void;
+  type: 'button'
+  onClick: () => void
 }
 type ItemProps = {
-    title: string;
-    icon: IconProps;
-    disabled?: boolean;
+  title: string
+  icon: IconProps
+  disabled?: boolean
 }
-type Props = (ItemProps & AnchorProps) | (ItemProps & ButtonProps);
+type Props = (ItemProps & AnchorProps) | (ItemProps & ButtonProps)
 
-export const DynamicMenuItem = forwardRef<HTMLAnchorElement | HTMLButtonElement, Props>((props, ref) => {
-    const {type, icon, title} = props
+export const DynamicMenuItem = forwardRef<HTMLAnchorElement | HTMLButtonElement, Props>(
+  (props, ref) => {
+    const { type, icon, title } = props
     if (type === 'a') {
-        return (
-            <a href={props.route} className={s.link} ref={ref as React.Ref<HTMLAnchorElement>}>
-                <Icon iconId={icon}/>
-                <span>{title}</span>
-            </a>
-        );
+      return (
+        <a href={props.href} className={s.link} ref={ref as React.Ref<HTMLAnchorElement>}>
+          {/*<Icon iconId={icon}/>*/}
+          <span>{title}</span>
+        </a>
+      )
     } else {
-        return (
-            <button onClick={() => props.callback()} className={s.button} ref={ref as React.Ref<HTMLButtonElement>}>
-                <Icon iconId={icon}/>
-                <span>{title}</span>
-            </button>
-        );
+      return (
+        <button
+          onClick={() => props.onClick()}
+          className={s.button}
+          ref={ref as React.Ref<HTMLButtonElement>}
+        >
+          {/*<Icon iconId={icon}/>*/}
+          <span>{title}</span>
+        </button>
+      )
     }
-});
-
+  }
+)
