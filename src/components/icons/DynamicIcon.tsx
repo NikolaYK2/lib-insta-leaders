1568f8eb01a6cf09c9b96d011dd329a46e78751f
albogachiev <albogachiev-2014@mail.ru>
2024-09-06 10:46:02 +0300

    IL-62.2-Icons feat: Added DynamicIcon component

--- src/components/icons/DynamicIcon.tsx
+++ src/components/icons/DynamicIcon.tsx
@@ -1,22 +1,17 @@
-import { forwardRef, memo, SVGProps } from 'react'
+import { forwardRef, memo, Ref, SVGProps } from 'react'
 import * as icons from './iconsOrdinary'
 
 interface DynamicIconProps extends SVGProps<SVGSVGElement> {
   iconId: keyof typeof icons
 }
 
-// forwardRef позволяет передать реф от родительского компонента к элементу внутри компонента DynamicIcon
-const DynamicIcon = forwardRef<SVGSVGElement, DynamicIconProps>(({ iconId, ...props }, ref) => {
-  // Определяем компонент иконки на основе iconId
+const DynamicIcon = ({ iconId, ...props }: DynamicIconProps, ref: Ref<SVGSVGElement>) => {
   const IconComponent = icons[iconId]
 
-  // Если иконка с указанным iconId не найдена, возвращаем null
-  // Это предотвращает попытку рендеринга несуществующего компонента
   if (!IconComponent) return null
 
-  // Возвращаем компонент иконки с переданным ref и остальными пропсами
-  // ref используется для получения ссылки на DOM элемент иконки
   return <IconComponent {...props} ref={ref} />
-})
+}
 
-export default memo(DynamicIcon)
+const ForwardRef = forwardRef(DynamicIcon)
+export default memo(ForwardRef)
