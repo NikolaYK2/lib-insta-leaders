60267d0754426aff116f87ccae41c3e74affdf48
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-09-08 22:02:38 +0400

    fix: DynamicIcons

---
+++ src/components/icons/iconComponents/Email.tsx
@@ -0,0 +1,5 @@
+import type { SVGProps } from "react";
+import { Ref, forwardRef } from "react";
+const SvgEmail = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" ref={ref} {...props}><g clipPath="url(#email_svg__a)"><path fill="currentColor" d="M19 4H5a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3m0 2-6.5 4.47a1 1 0 0 1-1 0L5 6z" /></g><defs><clipPath id="email_svg__a"><path fill="currentColor" d="M0 0h24v24H0z" /></clipPath></defs></svg>;
+const ForwardRef = forwardRef(SvgEmail);
+export default ForwardRef;
