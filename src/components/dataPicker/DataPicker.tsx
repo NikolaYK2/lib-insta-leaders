1a22464203450553cc17468b5fdaaebb45a98cb1
k2 <izmail.k2@gmail.com>
2024-09-10 20:08:45 +0300

    refactor: Updated DatePicker component structure and logic
    - Replaced the old DatePickerDemo component with a new DatePicker component.
    - Added support for selecting both single dates and date ranges.
    - Integrated `Popover`, `Button`, `Calendar`, and `DynamicIcon` components.
    - Implemented error handling and conditional error message display.
    - Updated UI styling with `DataPicker.module.scss` and dynamic button states.

--- src/components/dataPicker/DataPicker.tsx
+++ src/components/dataPicker/DataPicker.tsx
@@ -1,32 +1,60 @@
-'use client'
+import { Popover, PopoverContent, PopoverTrigger } from '../porover/Porover'
+import { useState } from 'react'
+import { format } from 'date-fns'
+import { Button } from '../button'
+import { Calendar } from '../calendar/Calendar'
+import { DateRange } from 'react-day-picker'
+import s from './DataPicker.module.scss'
+import { Typography } from '../typography'
+import DynamicIcon from '../icons/DynamicIcon'
+import clsx from 'clsx'
 
-import * as React from 'react'
-// import { format } from 'date-fns'
-// import { Calendar as CalendarIcon } from 'lucide-react'
-//
-// import { Button } from '@/components/ui/button'
-// import { Calendar } from '@/components/ui/calendar'
-import { Popover, PopoverTrigger, PopoverContent } from '../porover/Porover'
+type DataPickerProps = {
+  error?: string
+}
+export const DatePicker = ({ error = false }: DataPickerProps) => {
+  const [date, setDate] = useState<DateRange | Date | undefined>(undefined)
+  const [isActive, setIsActive] = useState(false)
+  // Функция для отображения выбранной даты или диапазона
 
-export function DatePickerDemo() {
-  const [date, setDate] = React.useState<Date>()
+  const renderDateLabel = () => {
+    if (!date) {
+      return <span>Pick a date</span>
+    }
 
+    if (typeof date === 'object' && 'from' in date && date.to) {
+      // Если выбран диапазон
+      return (
+        <>
+          {format(date.from, 'MM/dd/yyyy')} - {format(date.to, 'MM/dd/yyyy')}
+        </>
+      )
+    }
+
+    // Если выбрана одна дата
+    return format(date as Date, 'MM/dd/yyyy')
+  }
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        {/*<Button*/}
-        {/*  variant={'outline'}*/}
-        {/*  // className={cn(*/}
-        {/*  //   'w-[280px] justify-start text-left font-normal',*/}
-        {/*  //   !date && 'text-muted-foreground'*/}
-        {/*  // )}*/}
-        {/*>*/}
-        {/*  <CalendarIcon />*/}
-        {/*  {date ? format(date, 'PPP') : <span>Pick a date</span>}*/}
-        {/*</Button>*/}
+    <Popover open={isActive} onOpenChange={setIsActive}>
+      <PopoverTrigger asChild disabled={false}>
+        <Button
+          variant={'outline'}
+          className={clsx(s.buttonTrigger, isActive && s.activeBtnTrigger, error && s.error)}
+        >
+          <Typography variant={'regular_text_16'}>{renderDateLabel()}</Typography>
+          <DynamicIcon
+            iconId={isActive ? 'Calendar' : 'CalendarOutline'}
+            className={s.iconTrigger}
+          />
+        </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
-        {/*<Calendar mode="single" selected={date} onSelect={setDate} initialFocus />*/}
+      {error && (
+        <Typography variant={'small_text'} className={s.errorMessage}>
+          {error}
+        </Typography>
+      )}
+      <PopoverContent className={s.popoverContent} sideOffset={0}>
+        <Calendar onSelect={setDate} selected={date} />
       </PopoverContent>
     </Popover>
   )
