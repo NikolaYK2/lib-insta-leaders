feac92c1f67e51a3c7294e8d1ab71a8bca9d3f7a
klrotex <klrotex11@gmail.com>
2024-09-11 15:39:05 +0300

    fix: Slider, Table displayName

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -15,20 +15,25 @@ export const TableHeader = forwardRef<
 >(({ className, ...props }, ref) => (
   <thead ref={ref} className={clsx(s.tHeader, className)} {...props} />
 ))
+TableHeader.displayName = 'TableHeader'
 
 export const TableBody = forwardRef<
   HTMLTableSectionElement,
   HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => <tbody ref={ref} className={className} {...props} />)
+TableBody.displayName = 'TableBody'
 
 export const TableRow = forwardRef<HTMLTableRowElement, HTMLAttributes<HTMLTableRowElement>>(
   ({ className, ...props }, ref) => <tr ref={ref} className={clsx(s.tr, className)} {...props} />
 )
+TableRow.displayName = 'TableRow'
 
 export const TableHead = forwardRef<HTMLTableCellElement, ThHTMLAttributes<HTMLTableCellElement>>(
   ({ className, ...props }, ref) => <th ref={ref} className={clsx(s.tHead, className)} {...props} />
 )
+TableHead.displayName = 'TableHead'
 
 export const TableCell = forwardRef<HTMLTableCellElement, TdHTMLAttributes<HTMLTableCellElement>>(
   ({ className, ...props }, ref) => <td ref={ref} className={clsx(s.tCell, className)} {...props} />
 )
+TableCell.displayName = 'TableCell'
