ef190f06e83c8040bc7b4cbcb930d08a4ece2861
k2 <izmail.k2@gmail.com>
2024-09-14 21:59:14 +0300

    feat: Add PickerCalendar - common component for reuse

---
+++ src/components/picker/PickerCalendar.tsx
@@ -0,0 +1,70 @@
+import React, { ReactNode } from 'react'
+import clsx from 'clsx'
+import { Popover, PopoverTrigger } from '@radix-ui/react-popover'
+import { Button } from '../button'
+import { DynamicIcon } from '../icons'
+import { Typography, TypographyVariant } from '../typography'
+import { PopoverContent } from '../porover'
+import Calendar, { CalendarProps } from '../calendar/Calendar'
+import s from './PickerCalendar.module.scss'
+
+export type PickerCalendarProps = CalendarProps & {
+  isActive?: boolean
+  setIsActive: (isActive: boolean) => void
+  className?: string
+  error?: ReactNode
+  title?: ReactNode
+  sideOffsetContent?: number
+}
+export const PickerCalendar = ({
+  isActive,
+  setIsActive,
+  title,
+  error,
+  className = '',
+  setMouth,
+  options = true,
+  mouth,
+  selected,
+  onSelect,
+  disabled = false,
+  sideOffsetContent = 0,
+}: PickerCalendarProps) => {
+  return (
+    <Popover open={isActive} onOpenChange={setIsActive}>
+      <PopoverTrigger asChild disabled={disabled}>
+        <Button
+          variant={'outline'}
+          className={clsx(
+            s.buttonTrigger,
+            isActive && s.activeBtnTrigger,
+            disabled && s.triggerDisabled,
+            error && s.error,
+            className
+          )}
+        >
+          {title && <Typography variant={TypographyVariant.bold_text_16}>{title}</Typography>}
+          <DynamicIcon
+            iconId={isActive ? 'Calendar' : 'CalendarOutline'}
+            className={clsx(s.iconTrigger)}
+          />
+        </Button>
+      </PopoverTrigger>
+      <Typography
+        variant={TypographyVariant.small_text}
+        className={clsx(s.errorMessage, mouth && s.mod)}
+      >
+        <small>{error}</small>
+      </Typography>
+      <PopoverContent className={s.popoverContent} sideOffset={sideOffsetContent}>
+        <Calendar
+          onSelect={onSelect}
+          selected={selected}
+          mouth={mouth}
+          setMouth={setMouth}
+          options={options}
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}
