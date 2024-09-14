ab1a52278e56f4e96a41a817472f72f84906c5cd
k2 <izmail.k2@gmail.com>
2024-09-14 15:37:37 +0300

    refactor: add check for Date | DataRange - import connection - default import due to name conflict with icons

--- src/components/dataPicker/DataPicker.tsx
+++ src/components/dataPicker/DataPicker.tsx
@@ -2,46 +2,52 @@ import { Popover, PopoverContent, PopoverTrigger } from '../porover/Porover'
 import { useState } from 'react'
 import { format } from 'date-fns'
 import { Button } from '../button'
-import { Calendar } from '../calendar/Calendar'
+import Calendar from '../calendar/Calendar'
 import { DateRange } from 'react-day-picker'
 import s from './DataPicker.module.scss'
-import { Typography } from '../typography'
+import { Typography, TypographyVariant } from '../typography'
 import DynamicIcon from '../icons/DynamicIcon'
 import clsx from 'clsx'
 
 type DataPickerProps = {
   error?: string
 }
-export const DatePicker = ({ error = false }: DataPickerProps) => {
-  const [date, setDate] = useState<DateRange | Date | undefined>(undefined)
+export const DatePicker = ({ error = '' }: DataPickerProps) => {
+  const [data, setData] = useState<DateRange | Date | undefined>()
   const [isActive, setIsActive] = useState(false)
   // Функция для отображения выбранной даты или диапазона
 
   const renderDateLabel = () => {
-    if (!date) {
+    if (!data) {
       return <span>Pick a date</span>
     }
-
-    if (typeof date === 'object' && 'from' in date && date.to) {
-      // Если выбран диапазон
-      return (
-        <>
-          {format(date.from, 'MM/dd/yyyy')} - {format(date.to, 'MM/dd/yyyy')}
-        </>
-      )
+    if ('from' in data && data.from instanceof Date) {
+      // Если это DateRange и выбран диапазон (есть и from, и to)
+      if (data.to) {
+        return (
+          <>
+            {format(data.from, 'MM/dd/yyyy')} - {format(data.to, 'MM/dd/yyyy')}
+          </>
+        )
+      }
     }
 
-    // Если выбрана одна дата
-    return format(date as Date, 'MM/dd/yyyy')
+    // Если это просто одиночная дата (Date)
+    if (data instanceof Date) {
+      return format(data, 'MM/dd/yyyy')
+    }
+
+    return <span>Invalid date</span> // На случай ошибки
   }
+
   return (
     <Popover open={isActive} onOpenChange={setIsActive}>
-      <PopoverTrigger asChild disabled={false}>
+      <PopoverTrigger asChild>
         <Button
           variant={'outline'}
           className={clsx(s.buttonTrigger, isActive && s.activeBtnTrigger, error && s.error)}
         >
-          <Typography variant={'regular_text_16'}>{renderDateLabel()}</Typography>
+          <Typography variant={TypographyVariant.bold_text_16}>{renderDateLabel()}</Typography>
           <DynamicIcon
             iconId={isActive ? 'Calendar' : 'CalendarOutline'}
             className={s.iconTrigger}
@@ -49,12 +55,12 @@ export const DatePicker = ({ error = false }: DataPickerProps) => {
         </Button>
       </PopoverTrigger>
       {error && (
-        <Typography variant={'small_text'} className={s.errorMessage}>
+        <Typography variant={TypographyVariant.small_text} className={s.errorMessage}>
           {error}
         </Typography>
       )}
       <PopoverContent className={s.popoverContent} sideOffset={0}>
-        <Calendar onSelect={setDate} selected={date} />
+        <Calendar onSelect={setData} selected={data} />
       </PopoverContent>
     </Popover>
   )
