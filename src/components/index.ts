8ff9ca25ecc5b376db5a8e5eb98c83c4dd873cc9
albogachiev <albogachiev-2014@mail.ru>
2024-09-16 17:15:11 +0300

    IL-51 feat: index for Recaptcha

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,7 +4,7 @@ export * from './cardTest'
 export * from './input'
 export * from './label'
 export * from './tabs'
-
+export * from './recaptcha'
 export * from './card'
 export * from './cardTest'
 export * from './dropDownMenu'
