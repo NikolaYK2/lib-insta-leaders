c59ef37661e90282e89b1e42bc798e9b134e8505
albogachiev <albogachiev-2014@mail.ru>
2024-10-11 21:33:18 +0300

    IL-99 feat: create a component Toast

---
+++ src/components/toast/Toast.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode } from 'react'
+import s from './style.module.scss'
+import { Typography, TypographyVariant } from '../typography'
+import { DynamicIcon } from '../icons'
+
+type Props = {
+  variant: 'success' | 'error' | 'alertError'
+  children?: ReactNode
+  className?: string
+  maxWidth?: string
+  callback?: () => void
+}
+export const Toast = ({ callback, children, variant, className, maxWidth = '325px' }: Props) => {
+  const onClose = () => {
+    if (callback) callback()
+  }
+  return (
+    <div className={`${s[variant]} ${className}`} style={{ maxWidth }}>
+      <Typography variant={TypographyVariant.regular_text_16}>
+        <span>Error! </span> {children}
+      </Typography>
+      {variant !== 'error' && (
+        <DynamicIcon iconId={'Close'} width={24} height={24} onClick={onClose} />
+      )}
+    </div>
+  )
+}
